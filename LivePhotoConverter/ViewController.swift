99417aea88910b4ad4781124322e699d82443004
Nikita Zhukov <zhukov@vigroup.ru>
2017-08-16 11:18:50 +0500

    init

---
+++ LivePhotoConverter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LivePhotoConverter
+//
+//  Created by Nikita Zhukov on 16/8/17.
+//  Copyright © 2017 Nikita Zhukov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
